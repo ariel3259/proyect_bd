f28a553c2ab33c865f163ee60fe0e93d9e438a2e
ariel3259 <susanaarcia48@gmail.com>
2021-06-22 16:37:38 -0300

    Se creo el programa

---
+++ school/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace school
+{
+    public class Person
+    {
+        private int id;
+        private string name;
+        public string lastname;
+        public string email;
+        public string password;
+        public string nickname;
+        public string create_at;
+        public string update_at;
+
+        public Person(int id,string name, string lastname, string email, string password, string nickname, string create_at,string update_at)
+        {
+            this.id = id;
+            this.name = name;
+            this.lastname = lastname;
+            this.email = email;
+            this.password = password;
+            this.nickname = nickname;
+            this.create_at = create_at;
+            this.update_at = update_at;
+        }
+        public override string ToString() => ("Nombre: "+this.name+"\nApellido: "+this.lastname+"\nCorreo: "+this.email+"\nContraseña: "+this.password);
+    }
+}
---
+++ school/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace school
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Person p = new Person(1, "Ariel", "Santangelo", "susanarcia@gmail.com", "1234", "ariel3259", "06/22/21", "06/22/21");
+            Console.WriteLine(p.ToString());
+        }
+    }
+
+}
